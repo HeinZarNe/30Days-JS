226618e00f273b6a204cc4727f8df3c4c73abc43
LORDxisserious <91208047+LORDxisserious@users.noreply.github.com>
2021-12-22 16:04:59 +0630

    day5

---
+++ Day-5(The-image-panel)/day5.js
@@ -0,0 +1,16 @@
+let panels = document.querySelectorAll(".panel")
+
+function toggleOpen() {
+
+    this.classList.toggle('clicked');
+}
+
+function toggleActive(e) {
+    console.log(e.propertyName);
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
